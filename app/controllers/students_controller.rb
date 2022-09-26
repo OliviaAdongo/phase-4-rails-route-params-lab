a3568953dfa436a66c5d5e08411c9a40a95bcc40
Olivia Adongo <akinyioliviatove@gmail.com>
2022-09-26 12:41:47 +0300

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,8 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
-
+def show
+  person = Student.find(params[:id])
+  render json: person
+end
 end
